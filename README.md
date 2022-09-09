be3c6169a0c0ad6bdc6346b15ef548dce76842b8
eduardossh <113209179+eduardossh@users.noreply.github.com>
2022-09-09 22:09:59 +0000

    installscriptssh

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # SSH-PLUS
 
-# @ALFAINTERNET
+# @eduardossh
 
 *PROJETO EM ANDAMENTO...
 
--- Sistema/open.py
+++ Sistema/open.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# SSHPLUS By @ALFAINTERNET
+# SSHPLUS By @eduardossh
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
